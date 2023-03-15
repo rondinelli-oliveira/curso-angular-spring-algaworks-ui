335b24ace439dd52d5b9cbe23812f262c2099bb9
rondinelli-oliveira <rondinelliads@gmail.com>
2023-03-14 21:35:19 -0300

    configuring lazy pagination

--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LazyLoadEvent } from 'primeng/api';
+
 import { LancamentoFiltro, LancamentoService } from './../lancamento.service';
 
 @Component({
@@ -11,19 +13,27 @@ export class LancamentosPesquisaComponent implements OnInit {
 
   filtro = new LancamentoFiltro();
 
+  totalRegistros: number = 0
+
   lancamentos: any[] = [];
 
   constructor(private lancamentoService: LancamentoService) { }
 
-  ngOnInit(): void {
-    this.pesquisar();
+  ngOnInit() {
   }
 
-  pesquisar(): void {
+  pesquisar(pagina: number = 0): void {
+    this.filtro.pagina = pagina;
 
     this.lancamentoService.pesquisar(this.filtro)
-      .then(resultado => {
+      .then((resultado: any) => {
         this.lancamentos = resultado.lancamentos;
+        this.totalRegistros = resultado.total;
       });
+  }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event!.first! / event!.rows!;
+    this.pesquisar(pagina);
   }
 }
